10b42244804ccc1dea44d8c3d9fd445f9135ab62
Slavik <theslavprod@gmail.com>
2023-06-07 16:42:08 +0400

    added new task

---
+++ JS_Tasks/getThirdElementFromNumbersArray.js
@@ -0,0 +1,15 @@
+const arr = [1,2,9,66, 66,5,7]
+
+function getThirdElementFromNumbersArray(arr) {
+const isNumber = arr.every((elem) => typeof elem === 'number' )
+ if (!isNumber) {
+ 	return "Array items bad"
+ }
+	const newArr = [...new Set(arr)]
+	newArr.sort((a,b) => a - b )
+  console.log(newArr)
+  return newArr[newArr.length - 3]
+}
+
+console.log(getThirdElementFromNumbersArray(arr))
+
