73274d1d3fa4c78410594a7394d68f321b23eeae
Slavik <theslavprod@gmail.com>
2023-06-07 17:05:33 +0400

    added new task

---
+++ JS_Tasks/factorialWithWhile.js
@@ -0,0 +1,11 @@
+function Factorial(num) {
+  let result = num;
+  if (num === 0 || num === 1) return 1;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
+}
+
+console.log(Factorial(6));
