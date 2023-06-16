6bf43cfe488b9faf36c6ac365cadc891871dbbee
Slavik <theslavprod@gmail.com>
2023-06-16 17:32:32 +0400

    adding bubble sort

---
+++ algoritms/4_bubble_sort.js
@@ -0,0 +1,16 @@
+const numbers = [6, 2, -2, 6, -2, 7, -6, 45, 25, -78];
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j + 1] < arr[j]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(numbers));
